8c95168ae70d985043f690cca6d44455007f7db3
Gonzalo Lezcano <78862648+Gonzo1986@users.noreply.github.com>
2023-01-31 10:54:18 -0300

    Add files via upload

---
+++ js/funtion.js
@@ -0,0 +1,8 @@
+document.getElementById('boton').onclick = function(){
+	console.log("contacto");
+	document.getElementById("demo").innerHTML = "Celular:156-1234567
+                Email: tpfinal@ticmas.com.ar";
+
+
+
+}
---
+++ js/js.js
@@ -0,0 +1,8 @@
+document.getElementById("boton").onclick = function(){
+	console.log("contacto");
+	document.getElementById("demo").innerHTML = "Celular: <span class="tel">156-1234567</span><br />
+                Email: <a class="email" href="mailto:tpfinal@ticmas.com.ar">tpfinal@ticmas.com.ar</a>"
+
+
+
+}
